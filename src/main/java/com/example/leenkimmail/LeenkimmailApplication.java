90c5f16a8e5b46e0211d0cb86c93d3830c5ec554
DESKTOP-P2O3E9H\hoho3 <hoho3419@naver.com>
2023-10-26 19:23:36 +0900

    leenkim 프로젝트 기본 설정

---
+++ src/main/java/com/example/leenkimmail/LeenkimmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.leenkimmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeenkimmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeenkimmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leenkimmail/LeenkimmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leenkimmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeenkimmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
