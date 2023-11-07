394feed2c8c369cf2a3a8e5d9d18da04cce071cc
DESKTOP-P2O3E9H\hoho3 <hoho3419@naver.com>
2023-11-07 18:16:29 +0900

    Theme 을 추가해서 메인 색상 전역으로 분배

--- src/main/fronted/src/main.jsx
+++ src/main/fronted/src/main.jsx
@@ -4,14 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./style/them.jsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </>
 );
---
+++ src/main/fronted/src/style/them.jsx
@@ -0,0 +1,9 @@
+const color = {
+  main: "#0c4da2",
+};
+
+const theme = {
+  color,
+};
+
+export default theme;
