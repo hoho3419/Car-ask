e6da5d8444752ec5dc5339d339e332db6b8c84aa
DESKTOP-P2O3E9H\hoho3 <hoho3419@naver.com>
2023-11-08 18:36:30 +0900

    토큰이 없으면 접근하지 못하게 변경

--- src/main/fronted/src/App.jsx
+++ src/main/fronted/src/App.jsx
@@ -6,6 +6,7 @@ import AdminLogin from "./pages/AdminLogin";
 import AdminLayout from "./components/UI/AdminLayout";
 import EmailList from "./components/admin/EmailList";
 import EmailDetail from "./pages/EmailDetail";
+import { PrivateRoute, ProtectedRoute } from "./route";
 
 function App() {
   return (
@@ -13,8 +14,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/privacy" element={<Privacy />}></Route>
-        <Route path="/adm" element={<AdminLogin />}></Route>
-        <Route path="/adm" element={<AdminLayout />}>
+        <Route
+          path="/adm"
+          element={<ProtectedRoute component={<AdminLogin />} />}
+        ></Route>
+        <Route
+          path="/adm"
+          element={<PrivateRoute component={<AdminLayout />} />}
+        >
           <Route path="email" element={<EmailList />} />
           <Route path="email/:id" element={<EmailDetail />} />
         </Route>
