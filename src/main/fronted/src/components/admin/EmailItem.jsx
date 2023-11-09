ed58ad5d409233db818c7c75dd96eb20c7982e1e
DESKTOP-P2O3E9H\hoho3 <hoho3419@naver.com>
2023-11-09 18:28:59 +0900

    Admin List Item 문의 리스트와 클릭하면 해당 문의 보여주는 Detail 로 이동

--- src/main/fronted/src/components/admin/EmailItem.jsx
+++ src/main/fronted/src/components/admin/EmailItem.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { formatDate } from "../../util";
+import { Link } from "react-router-dom";
 
-const EmailItem = () => {
-  return <td></td>;
+const EmailItem = (props) => {
+  const { id, name, brand, regDate, identity, contactTime } = props;
+  const formattedDateTime = formatDate(regDate);
+
+  return (
+    <tbody>
+      <TableCell>
+        <td>
+          <Link to={`/adm/email/${id}`}>{name}</Link>
+        </td>
+        <td>
+          <Link to={`/adm/email/${id}`}>{identity}</Link>
+        </td>
+        <td>
+          <Link to={`/adm/email/${id}`}>{brand}</Link>
+        </td>
+        <td>
+          <Link to={`/adm/email/${id}`}>{contactTime + "시"}</Link>
+        </td>
+        <td>
+          <Link to={`/adm/email/${id}`}>{formattedDateTime}</Link>
+        </td>
+      </TableCell>
+    </tbody>
+  );
 };
 
 export default EmailItem;
+
+const TableCell = styled.tr`
+  td {
+    border: 1px solid #ddd;
+    padding: 0.5rem;
+  }
+`;
--- src/main/fronted/src/components/admin/EmailList.jsx
+++ src/main/fronted/src/components/admin/EmailList.jsx
@@ -1,46 +1,58 @@
 import React from "react";
 import EmailItem from "./EmailItem";
-
-const demo = [
-  {
-    id: 1,
-    clientName: "ㅇㅁㄴ",
-    brand: "BMW",
-    regDate: "2023-11-04T23:59:12.37679",
-  },
-  {
-    id: 2,
-    clientName: "ㅇㅁㄴ",
-    brand: "BMW",
-    regDate: "2023-11-05T00:00:55.36701",
-  },
-  {
-    id: 3,
-    clientName: "ㅇㅁㄴ",
-    brand: "BMW",
-    regDate: "2023-11-05T00:07:40.025557",
-  },
-];
+import styled from "@emotion/styled";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEamils } from "../../api";
+import Spiner from "../UI/Spiner";
 
 const EmailList = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["email"],
+    queryFn: async ({ signal }) => await fetchEamils({ signal }),
+  });
+
   return (
-    <table>
-      <tr>
-        <th>이름</th>
-        <th>브랜드</th>
-        <th>문의 날짜</th>
-      </tr>
-      {demo.map((el) => (
-        <EmailItem
-          key={el.id}
-          id={el.id}
-          name={el.clientName}
-          brand={el.brand}
-          regDate={el.regDate}
-        />
-      ))}
-    </table>
+    <TableContainer>
+      <thead>
+        <TableHeaderCell>
+          <th>이름</th>
+          <th>신분</th>
+          <th>브랜드</th>
+          <th>연락가능 시간</th>
+          <th>문의 날짜</th>
+        </TableHeaderCell>
+      </thead>
+      {data &&
+        data.map((el) => (
+          <EmailItem
+            key={el.id}
+            id={el.id}
+            name={el.clientName}
+            brand={el.brand}
+            regDate={el.regDate}
+            identity={el.identity}
+            contactTime={el.contactTime}
+          />
+        ))}
+      {isLoading && <Spiner />}
+    </TableContainer>
   );
 };
 
 export default EmailList;
+
+const TableContainer = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  margin: 2rem;
+`;
+
+const TableHeaderCell = styled.tr`
+  th {
+    background-color: #f2f2f2;
+    border: 1px solid #ddd;
+    padding: 10px;
+    text-align: left;
+    font-weight: bold;
+  }
+`;
